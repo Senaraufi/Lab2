869a66aa1ef37c0e7116264eca74c6f417970c56
sena <88288148+Senaraufi@users.noreply.github.com>
2025-03-23 12:04:18 +0000

    Done

--- public/delete.php
+++ public/delete.php
@@ -1,12 +1,25 @@
 <?php
-
-/*    
- * List all users
+/**
+* Delete a user
 */
+require "../common.php";
+if (isset($_GET["id"])) {
+    try {
+    require_once '../src/DBconnect.php';
+        $id = $_GET["id"];
+        $sql = "DELETE FROM users WHERE id = :id";
+        $statement = $connection->prepare($sql);
+        $statement->bindValue(':id', $id);
+        $statement->execute();
 
+        $success = "User ". $id. " successfully deleted";
+
+}   catch(PDOException $error) {
+    echo $sql . "<br>" . $error->getMessage();
+    }
+}
 try {
-    require "../common.php";
-        require_once '../src/DBconnect.php';
+    require_once '../src/DBconnect.php';
 
     $sql = "SELECT * FROM users";
 
@@ -14,17 +27,16 @@ try {
     $statement->execute();
 
     $result = $statement->fetchAll();
-    
 } catch(PDOException $error) {
     echo $sql . "<br>" . $error->getMessage();
 }
-    
 ?>
-
 <?php require "templates/header.php"; ?>
-
-<h2>Update users</h2>
-
+    
+<h2>Delete users</h2>
+    
+<?php if ($success) echo $success; ?>
+    
 <table>
     <thead>
         <tr>
@@ -35,13 +47,13 @@ try {
             <th>Age</th>
             <th>Location</th>
             <th>Date</th>
-            <th>Edit</th>
+            <th>Delete</th>
         </tr>
     </thead>
 <tbody>
 
-<?php foreach ($result as $row) : ?>
-    <tr>
+    <?php foreach ($result as $row) : ?>
+        <tr>
         <td><?php echo escape($row["id"]); ?></td>
         <td><?php echo escape($row["firstname"]); ?></td>
         <td><?php echo escape($row["lastname"]); ?></td>
@@ -49,11 +61,15 @@ try {
         <td><?php echo escape($row["age"]); ?></td>
         <td><?php echo escape($row["location"]); ?></td>
         <td><?php echo escape($row["date"]); ?> </td>
-        <td><a href="update-single.php?id=<?php echo escape($row["id"]); ?>">Edit</a></td>
-        <td><a href="delete.php?id=<?php echo escape($row["id"]); ?> "> Delete </a></td>
+        <td><a href="delete.php?id=<?php echo escape($row["id"]);
+    ?>">Delete</a></td>
+    
     </tr>
-<?php endforeach; ?>
+        <?php endforeach; ?>
 </tbody>
 </table>
 
-<a href="index.php">Back to home</a>
+    <a href="index.php">Back to home</a>
+   
+    <?php require "templates/footer.php"; ?>
+    
